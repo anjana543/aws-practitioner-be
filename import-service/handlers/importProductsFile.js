f6f1a3bc3dec72a4bee34fd49b07339bba6f9c3d
V-EPAM-Anjana Nair <V-EPAM-Anjana.Nair@metrolinx.com>
2024-04-14 12:40:21 +0530

    add swagger

--- import-service/handlers/importProductsFile.js
+++ import-service/handlers/importProductsFile.js
@@ -1,35 +1,34 @@
 const AWS = require('aws-sdk');
-
 const { BUCKET, UPLOAD_PATH, REGION } = process.env;
 
 export default async function getSignedUrlForPut(fileName) {
   console.log('fileName: ', fileName);
-
   const s3 = new AWS.S3({ region: REGION });
 
-  try {
-    const statusCode = 200;
+  const params = {
+    Bucket: BUCKET,
+    Key: `${UPLOAD_PATH}${fileName}`,
+    Expires: 60,
+    ContentType: 'text/csv'
+  };
 
-    const params = {
-      Bucket: BUCKET,
-      Key: `${UPLOAD_PATH}/${fileName}`,
-      Expires: 60,
-      ContentType: 'text/csv'
-    };
+  return new Promise((resolve, reject) => {
+    s3.getSignedUrlPromise('putObject', params, (error, url) => {
+      console.log('url ', url);
 
-    const url = await s3.getSignedUrlPromise('putObject', params);
-    console.log('url ', url);
-
-    return {
-      statusCode: statusCode,
-      body: JSON.stringify(url)
-    };
-  } catch (error) {
-    console.error('Error:', error);
-
-    return {
-      statusCode: 500,
-      body: JSON.stringify(error)
-    };
-  }
-}
+      if (url) {
+        resolve({
+          statusCode: 200,
+          body: JSON.stringify(url)
+        });
+      } else {
+        console.error('Error');
+        console.error(error);
+        reject({
+          statusCode: 500,
+          body: JSON.stringify(error)
+        });
+      }
+    })
+  })
+};
